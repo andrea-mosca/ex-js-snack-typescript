84cd9640c26180a2ec80d6efe752e6bc752440d9
andrea-mosca <andrea.moscateli@gmail.com>
2025-09-04 10:58:31 +0200

    bonus snack 1

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,13 @@ if(typeof valore==="string"){
   console.log(valore.toUpperCase())
   }else if(typeof valore==="number"){
     console.log(valore*2);
-  }else if(typeof valore ==="boolean"){console.log(valore);
+  }else if(typeof valore ==="boolean"){console.log(valore?"Si":"No");
+  }else if(valore === null){
+    console.log("il dato è vuoto");
+  }else if(valore instanceof Array){console.log(valore.length);
+  }
+  else if(valore instanceof Promise){
+    valore.then((value)=>console.log(value));
+
   }else console.log("Valore non supportato");
 
