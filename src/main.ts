ac2bd67602b127cc9cef00488400b2c2e0ad72ad
andrea-mosca <andrea.moscateli@gmail.com>
2025-09-04 11:02:04 +0200

    snack 2

--- src/main.ts
+++ src/main.ts
@@ -14,3 +14,11 @@ if(typeof valore==="string"){
 
   }else console.log("Valore non supportato");
 
+// snack 2
+type Dipendente={
+  nome:string,
+  cognome:string,
+  annoNascita:number,
+  sesso: "m"|"f",
+  anniDiServizio:number[]
+}
